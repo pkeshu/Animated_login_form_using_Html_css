5d13aaa23557368c3fcd24f4d58fb3bb37d00ad9
Keshar <paudelkeshar2055@gmail.com>
2019-02-22 09:54:24 +0545

    animated login form using only html and css

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload=load;
+function load(){
+    document.getElementsByClassName("box").onClick=handaling;
+
+}
+function handaling(){
+    alert("its okay");
+}
+// handaling=(event)=>{
+//     alert("Its okay");
+// }
